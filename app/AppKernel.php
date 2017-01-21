a9ede28d4c3c03d13bd026c1afb552794bb60c96
NunoPress LLC <hello@nunopress.com>
2017-01-21 13:33:09 +0100

    Small updates

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -66,7 +66,13 @@ class AppKernel extends Kernel
             $bundles[] = new \Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new \Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new \Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new \Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            
+            if ('test' !== $this->getEnvironment()) {
+                $bundles[] = new \Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+                /* 3.3.x
+                $bundles[] = new \Symfony\Bundle\WebServerBundle\WebServerBundle();
+                */
+            }
         }
 
         return $bundles;
